006c3023598a11c92d9268b5b3163d3d1c3ca4df
KHKAC <rudgnsrb888@naver.com>
2025-11-06 13:52:20 +0900

    251106 PhotonManager

---
+++ Assets/Scripts/PhotonManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PhotonManager : MonoBehaviourPunCallbacks
+{
+    readonly string version = "1.0";
+    string userId = "Zack";
+
+    void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.GameVersion = version;
+        PhotonNetwork.NickName = userId;
+
+        Debug.Log(PhotonNetwork.SendRate);
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        //base.OnConnectedToMaster();
+        Debug.Log("Connected to Master!");
+        Debug.Log($"PhotonNetwork.InLobby = {PhotonNetwork.InLobby}");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        Debug.Log($"PhotonNetwork.InLobby = {PhotonNetwork.InLobby}");
+        PhotonNetwork.JoinRandomRoom();
+
+        // 랜덤 룸 입장에 실패할 경우의 함수
+
+    }
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log($"JoinRandom Failed {returnCode} : {message}");
+
+        RoomOptions ro = new RoomOptions();
+        ro.MaxPlayers = 20;
+        ro.IsOpen = true;
+        ro.IsVisible = true;
+
+        PhotonNetwork.CreateRoom("My Room", ro);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        //base.OnCreatedRoom();
+        Debug.Log("Created Room");
+        Debug.Log($"Room Name = {PhotonNetwork.CurrentRoom.Name}");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        //base.OnJoinedRoom();
+        Debug.Log($"PhotonNetwork.InRoom = {PhotonNetwork.InRoom}");
+        Debug.Log($"Player Count = {PhotonNetwork.CurrentRoom.PlayerCount}");
+
+        foreach (var player in PhotonNetwork.CurrentRoom.Players)
+        {
+            Debug.Log($"{player.Value.NickName} , {player.Value.ActorNumber}");
+        }
+    }
+}
